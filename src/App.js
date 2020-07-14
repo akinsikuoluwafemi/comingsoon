628d553c5919263df09a0ae95faf8a428a2a7238
Akinsiku Femi <akinsiku13@gmail.com>
2020-07-14 16:19:43 +0100

    Change moment date to iso format

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './components/Coundown/CountDown.scss';
+import './pages/ComingSoon/ComingSoon.scss';
 
-function App() {
+import ComingSoon from './pages/ComingSoon/ComingSoon';
+const App = () =>{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ComingSoon/>
+
     </div>
-  );
+  )
 }
 
+
 export default App;
+
+
+
---
+++ src/pages/ComingSoon/ComingSoon.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import './ComingSoon.scss';
+import CountDown from '../../components/Coundown/CountDown';
+import Gear from '../../images/gear-with-holes.svg';
+
+class ComingSoon  extends Component{
+    
+    state = {
+        countdown: {
+            futureDate: `2020-08-05T00:00:00`
+        }
+    }
+
+   render(){
+       const { countdown } = this.state;
+       
+       return (
+           <div className="bodee">
+               <div className="background">
+                   <CountDown futureDate={countdown.futureDate}></CountDown>
+                   
+                   <img src={Gear} className="cog" alt=""/>
+
+                   <p className="comingsoon-text"> We are Coming Soon</p>
+                   <button className="btnn ">BACK TO HOME</button>
+               </div>
+           </div>
+
+       )
+   }
+}
+
+
+export default ComingSoon;
